3144a8afefc4500c2f09c0d16e8af3f8dfcf0282
Oliver Chi <oliver.chi.ict@icloud.com>
2018-04-13 23:33:49 +1000

    init DataStore properties

--- twitterDepressionDetect/twitterDepressionDetect/DataStore.swift
+++ twitterDepressionDetect/twitterDepressionDetect/DataStore.swift
@@ -11,9 +11,16 @@ import CoreData
 
 class DataStore: NSObject {
     //properties
+    var twtterAccount: String;
+    var lastPostDate: Date;
+    var result: int_fast8_t;
     
-    //init
-    
+    //init with property values of lastPostDate and result
+    init(name: String, test: int_fast8_t, time: Date){
+        self.twtterAccount = name;
+        self.result = test;
+        self.lastPostDate = time;
+    }
     
     //method: store account name
     
