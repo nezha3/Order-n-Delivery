287a4e94e87290e0513f051dfa8fb318d605d9f1
Oliver Chi <oliver.chi.ict@icloud.com>
2018-04-16 00:35:48 +1000

    update launch screen and display time of it

--- twitterDepressionDetect/twitterDepressionDetect/AppDelegate.swift
+++ twitterDepressionDetect/twitterDepressionDetect/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        RunLoop.current.run(until: NSDate(timeIntervalSinceNow: 1) as Date) //Set launch screen display time as 1 second
         return true
     }
 
