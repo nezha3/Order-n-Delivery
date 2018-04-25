5b1b9ba51fe273813487fb5cd9cd43b9d89d9c04
Oliver Chi <oliver.chi.ict@icloud.com>
2018-04-25 10:29:40 +1000

    update

---
+++ twitterDepressionDetect/twitterDepressionDetect/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  twitterDepressionDetect
+//
+//  Created by Oliver Chi on 25/4/18.
+//  Copyright © 2018 Oliver Chi. All rights reserved.
+//
+
+import UIKit
+
+class Notification: NSObject {
+
+}
