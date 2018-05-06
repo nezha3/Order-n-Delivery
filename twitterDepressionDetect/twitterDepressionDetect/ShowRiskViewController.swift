5fdafd5f98815adac2086d0f4bd9f8ffe28c54b3
Oliver Chi <oliver.chi.ict@icloud.com>
2018-05-07 01:33:27 +1000

    update structure and titles

--- twitterDepressionDetect/twitterDepressionDetect/ShowRiskViewController.swift
+++ twitterDepressionDetect/twitterDepressionDetect/ShowRiskViewController.swift
@@ -2,24 +2,34 @@
 //  ShowRiskViewController.swift
 //  twitterDepressionDetect
 //
-//  Created by Oliver Chi on 6/5/18.
+//  Created by Oliver Chi on 7/5/18.
 //  Copyright © 2018 Oliver Chi. All rights reserved.
 //
 
 import UIKit
 
 class ShowRiskViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-       
+
+        // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-}
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
